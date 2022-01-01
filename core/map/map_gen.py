3a752624c53251e361df4ea5b56f5a57291fb335
linux-admin0001 <igorkorobigor@gmail.com>
2022-01-01 14:37:25 +0300

    da

---
+++ core/map/map_gen.py
@@ -0,0 +1,16 @@
+from . import Parser
+from random import randint
+
+
+class MapGenerator(object):
+    def __init__(self, filename, nx, ny):
+        """
+        nx, ny - count of tiles
+
+        filename - name of file
+        """
+        self.map = open
+    def gen(self):
+        for x in range(self.nx):
+            for y in range(self.ny):
+
