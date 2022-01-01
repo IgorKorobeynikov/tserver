2dba3fee64f91ed7ecb10ba7e3d3233ff0395102
linux-admin0001 <igorkorobigor@gmail.com>
2022-01-01 15:29:05 +0300

    some updates

--- core/map/__init__.py
+++ core/map/__init__.py
@@ -1,2 +1,3 @@
 from .loader import Loader
 from .map_parser import Parser
+from .map_gen import MapGenerator
--- core/map/map_gen.py
+++ core/map/map_gen.py
@@ -3,14 +3,20 @@ from random import randint
 
 
 class MapGenerator(object):
-    def __init__(self, filename, nx, ny):
+    def __init__(self, filename, nx, ny, parser=Parser()):
         """
         nx, ny - count of tiles
 
         filename - name of file
         """
-        self.map = open
-    def gen(self):
-        for x in range(self.nx):
-            for y in range(self.ny):
+        self.filename = filename
+        self.nx = nx
+        self.ny = ny
+        self.parser = parser
 
+    def gen(self):
+
+        with open(self.filename, "wb") as map:
+            for x in range(self.nx):
+                for y in range(self.ny):
+                    map.write(self.parser.dump((randint(0, 3), x*64, y*64)))
