b6bf7baf91024257e1fd003f9e73ff6918bb81ca
linux-admin0001 <igorkorobigor@gmail.com>
2022-04-07 15:55:17 +0300

    added BaseClientData, ClientDataNullKeys, ClientRequest, other upd

--- submodules/base_types.py
+++ submodules/base_types.py
@@ -1,4 +1,5 @@
-from typing import Any, TypedDict, Union, Dict, Optional
+from typing import Any, TypedDict, Dict, Union, Optional
+from typing import final
 
 __all__ = ("PlayerData", "ClientData", "ServerResponse")
 
@@ -9,18 +10,31 @@ class PlayerData(TypedDict):
     turret_rot: Union[int, float]
 
 
-class ClientData(TypedDict):
+class BaseClientData(TypedDict):
     key: str
     id: int
     addres: list[str, int]
     client_timeout_ms: int
 
+class ClientData(BaseClientData):
     player_data: Union[
         Dict,
         PlayerData
     ]
 
+class ClientDataNullKeys(ClientData):
+    """
+    ClientData with `key`: None
+    """
+    key: None
 
+@final
 class ServerResponse(TypedDict):
     status: int
     response: Optional[Any]
+
+class ClientRequest(TypedDict):
+    request: str
+    client_data: BaseClientData
+    player_data: PlayerData
+    request_body: Dict[str, Any]
