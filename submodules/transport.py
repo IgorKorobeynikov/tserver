592e7eb23dc104fcc83599a57dda78427afaf8af
linux-admin0001 <igorkorobigor@gmail.com>
2022-04-16 18:30:49 +0300

    fixed bug

--- submodules/transport.py
+++ submodules/transport.py
@@ -3,25 +3,27 @@ from .base_types import ClientRequest, ServerResponse
 from .ext_socket import UdpSocket
 
 from json import dumps, loads
-from typing import ByteString, Tuple
+from typing import ByteString, Dict, Tuple
 
 
 Port = int
 ClAddres = Tuple[str, Port]
 BytesAmount = int
+Packet = Dict # Will replaced in future
 
 class UdpTransport(UdpSocket, Transport):
     def __init__(self):
+        self.__total_recv = 0
+        self.__total_sent = 0
         super().__init__()
-
-    def sendto(self, packet: ServerResponse, addres: ClAddres) -> int:
+    def sendto(self, packet: Packet, addres: ClAddres) -> int:
 
         raw_packet: ByteString = dumps(packet).encode()
 
         self.__total_sent += len(raw_packet)
         return super().sendto(raw_packet, addres)
 
-    def recvfrom(self, buffersize: int) -> Tuple[ClientRequest, ClAddres]:
+    def recvfrom(self, buffersize: int) -> Tuple[Packet, ClAddres]:
         raw_packet, addr = super().recvfrom(buffersize)
         raw_packet: ByteString = raw_packet
         addres: ClAddres = addr
