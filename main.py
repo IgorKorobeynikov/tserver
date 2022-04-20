4e53e53756aa18253b3be345e1de1c2fbcd007fc
linux-admin0001 <igorkorobigor@gmail.com>
2022-04-20 19:49:21 +0300

    added some shit argparser

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from core.server import Server
+import argparse
+
+__DESC__ = "TwisteRServer - simple python game server on raw UDP sockets."
+
+# will be rewrited
+parser = argparse.ArgumentParser(description=__DESC__)
+parser.add_argument("--port", type=int, default=9265, help="Server port")
+parser.add_argument("--max_conns", type=int, default=100, help="Count of clients")
+parser.add_argument("--chat_size", type=int, default=10, help="Size of chat buffer")
+args = parser.parse_args()
 
 
 def main() -> None:
-    server: Server = Server()
+    server: Server = Server(**dict(args._get_kwargs()))
     try:
         server.run()
     except KeyboardInterrupt:
